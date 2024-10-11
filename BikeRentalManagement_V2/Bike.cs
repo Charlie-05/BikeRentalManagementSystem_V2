36fea698885fee4f57d97478a3fe212d883fa7e8
Paramananthan T. <UT01373@unicomtic.com>
2024-10-11 12:11:33 +0530

    Q10 and Q11

---
+++ BikeRentalManagement_V2/Bike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagement_V2
+{
+    internal class Bike
+    {
+        public string BikeId { get; set; }  
+        public string Brand {  get; set; }
+        public string Model { get; set; }
+        public decimal RentalPrice { get; set; }
+    }
+}
